5e601e88062bae9475071760b5686a822be097b0
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-10 12:51:54 -0300

    Config Database: PostgreSQL

--- backend/medicar/settings.py
+++ backend/medicar/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'medicar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'medicar',
+        'USER': 'medicar',
+        'PASSWORD': 'medicar',
+        'HOST': 'intmed_database_1',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
