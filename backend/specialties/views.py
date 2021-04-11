cdeab5e972ba0dd59c0ced885df305c067de95bf
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-11 11:45:24 -0300

    V 0.1

--- backend/specialties/views.py
+++ backend/specialties/views.py
@@ -10,14 +10,14 @@ class SpecialtyView(View):
         query_filter = request.GET.get('search')
         queryset = Specialty.objects.all()
         if query_filter:
-            queryset = queryset.filter(name__contains=query_filter)
+            queryset = queryset.filter(name=query_filter)
         serializer = SpecialtySerializer(queryset, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    def post(self, request):
-        data = JSONParser().parse(request)
-        serializer = SpecialtySerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    # def post(self, request):
+    #     data = JSONParser().parse(request)
+    #     serializer = SpecialtySerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(serializer.data, status=201)
+    #     return JsonResponse(serializer.errors, status=400)
