2fdfce4da7936a771013ab60f5e5b23b5a1e05f7
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-11 19:46:47 -0300

    Verificar o porque de cadastrar o médico apenas uma vez

--- backend/calendars/models.py
+++ backend/calendars/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from doctors.models import Doctor
+from datetime import datetime
 
-# Create your models here.
+class Day(models.Model):
+    date = models.DateField(verbose_name='Dia Disponível')
+    doctor = models.OneToOneField(Doctor, on_delete=models.CASCADE, verbose_name='Doutor(a)', unique=False)
+
+    def __str__(self):
+        return self.date.strftime("%Y/%m/%d") + " [" + self.doctor.name + "]" 
+
+    class Meta:
+        # constraints = [
+        #     models.UniqueConstraint(fields=['date', 'doctor'], name='Unique Doctor Date')
+        # ]
+        verbose_name = "Data"
+        verbose_name_plural = "Datas"
+
+class Hour(models.Model):
+    hour = models.TimeField(verbose_name='Horário Disponível')
+    day = models.ForeignKey(Day, on_delete=models.CASCADE, related_name='days', verbose_name='Dia')
+
+    # def __str__(self):
+    #     return datetime.strptime(self.hour, "%H:%M:%S")
+
+    
+    class Meta:
+        verbose_name = "Horário"
+        verbose_name_plural = "Horários"
