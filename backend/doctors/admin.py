8f635461ad7e1f684df20d2aa7d19051fe4a2090
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-15 16:11:09 -0300

    Substituido por uma App em pt-BR

--- backend/doctors/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Doctor
-
-class DoctorAdmin(admin.ModelAdmin):
-  exclude = ('created_at','updated_at','removed_at',)
-
-admin.site.register(Doctor, DoctorAdmin)
--- backend/specialties/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Specialty
-
-class SpecialtyAdmin(admin.ModelAdmin):
-  exclude = ('created_at','updated_at','removed_at',)
-
-admin.site.register(Specialty, SpecialtyAdmin)
