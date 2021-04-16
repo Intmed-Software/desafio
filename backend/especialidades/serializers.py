43e7e4e35f087c4fe059c7ae299b4d5af28bb8d9
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-16 17:11:14 -0300

    Removido

--- backend/especialidades/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Especialidade
-
-class EspecialidadeSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Especialidade
-    fields = ['id', 'name']
--- backend/medicos/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Medico
-from especialidades.serializers import EspecialidadeSerializer
-
-class MedicoSerializer(serializers.ModelSerializer):
-  especialidades = EspecialidadeSerializer(many=True)
-  class Meta:
-    model = Medico
-    fields = ['id', 'nome', 'crm', 'email', 'telefone', 'especialidades']
