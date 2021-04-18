81c56ddebcd8f5f59e8b4bd36410d538016323bf
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-18 19:06:03 -0300

    V0.1

--- backend/consultas/models.py
+++ backend/consultas/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
+from datetime import datetime
+from rest_framework.exceptions import ValidationError
+from agendas.models import Disponibilidade, Horario
 
-# Create your models here.
+class Consulta(models.Model):
+    disponibilidade = models.ForeignKey(Disponibilidade, on_delete=models.CASCADE, verbose_name='Disponibilidade')
+    horario = models.ForeignKey(Horario, on_delete=models.CASCADE, verbose_name='Horário')
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Usuário')
+    data_agendamento = models.DateTimeField(verbose_name='Data do Agendamento')
+
+    def __str__(self):
+        return str(self.disponibilidade) + " - " + str(self.horario)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['disponibilidade', 'horario'], name='Horário já reservado.')
+        ]
+        verbose_name = "Consulta"
+        verbose_name_plural = "Consultas"
+
+    def save(self, *args, **kwargs):
+        disponibilidade = Disponibilidade.objects.get(id=self.disponibilidade.id)
+        if disponibilidade.data < datetime.now().date():
+            raise ValidationError({"detail": "Não é possível marcar uma consulta em uma data no passado."})
+        horario = Horario.objects.get(id=self.horario.id)
+        if horario.hora < datetime.now().time():
+            raise ValidationError({"detail": "Não é possível marcar uma consulta em um horário no passado."})
+        self.data_agendamento = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        super().save(*args, **kwargs)
+
+    def delete(self):
+        disponibilidade = Disponibilidade.objects.get(id=self.disponibilidade.id)
+        if disponibilidade.data < datetime.now().date():
+            raise ValidationError({"detail": "Não é possível desmarcar uma consulta em uma data no passado."})
+        horario = Horario.objects.get(id=self.horario.id)
+        if horario.hora < datetime.now().time():
+            raise ValidationError({"detail": "Não é possível desmarcar uma consulta em um horário no passado."})
+        super().delete()
+
--- backend/consultas/views.py
+++ backend/consultas/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Consulta
+from agendas.models import Disponibilidade, Horario
+from datetime import datetime
+import json
 
-# Create your views here.
+
+class ConsultaList(APIView):
+    permission_classes = (IsAuthenticated,)
+    def get(self, request):
+        now = datetime.now()
+        queryset = Consulta.objects.filter(usuario=request.user.id).filter(disponibilidade__data__gte=now.date()).filter(horario__hora__gte=now.time()).order_by('disponibilidade__data', 'horario__hora')
+        return Response({
+            'id': consulta.id,
+            'dia': consulta.disponibilidade.data,
+            'horario': consulta.horario.hora,
+            'data_agendamento': consulta.data_agendamento,
+            'medico': consulta.disponibilidade.medico.nome
+        } for consulta in queryset)
+
+    def post(self, request):
+        disponibilidade = Disponibilidade.objects.get(id=request.data['agenda_id'])
+        horario = Horario.objects.filter(disponibilidade=request.data['agenda_id'], hora=request.data['horario']).first()
+        consulta = Consulta()
+        consulta.usuario = request.user
+        consulta.disponibilidade = disponibilidade
+        consulta.horario = horario
+        consulta.save()
+        return Response({
+            "id": consulta.id,
+            "dia": disponibilidade.data,
+            "horario": consulta.horario.hora,
+            "data_agendamento": consulta.data_agendamento,
+            "medico": {
+                "id": disponibilidade.medico.id,
+                "crm": disponibilidade.medico.crm,
+                "nome": disponibilidade.medico.nome,
+                'especialidade': [{
+                    'id': especialidade.id, 
+                    'name': especialidade.nome
+                } for especialidade in disponibilidade.medico.especialidades.all()]
+            }
+        })
+
+class ConsultaDetail(APIView):
+    permission_classes = (IsAuthenticated,)
+    def delete(self, request, id=None, format=None):
+        consulta = Consulta.objects.get(id=id)
+        consulta.delete()
+        return Response({})
