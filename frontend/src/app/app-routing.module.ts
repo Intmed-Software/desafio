80021d37e91f17d9952a0c70bee10cfb013cc3c2
Diego Feitosa <dev.diego.afc@gmail.com>
2021-04-29 17:08:05 -0300

    UPDATED: Removido Rotas sem uso

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './cliente/login/login.component';
-import { EspecialidadeComponent } from './especialidade/especialidade.component';
-import { MedicoComponent } from './medico/medico.component';
 import { ClienteCadastroComponent } from './cliente/cliente-cadastro/cliente-cadastro.component'
 import { ConsultaListaComponent } from './consulta/consulta-lista/consulta-lista.component';
 import { ConsultaCadastroComponent } from './consulta/consulta-cadastro/consulta-cadastro.component';
@@ -13,8 +11,6 @@ const routes: Routes = [
   { path: 'conta/cadastrar', component: ClienteCadastroComponent },
   { path: 'consultas', component: ConsultaListaComponent},
   { path: 'consultas/cadastrar', component:ConsultaCadastroComponent },
-  { path: 'especialidades', component: EspecialidadeComponent},
-  { path: 'medicos', component: MedicoComponent},
 ];
 
 @NgModule({
